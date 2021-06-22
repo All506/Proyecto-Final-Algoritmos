57afb7ecaa6ef967bbf9075192a2ba426a2c547e
juanp <juanp@DESKTOP-U5R5263>
2021-06-21 23:28:27 -0600

    Cambios-Juanpa

--- ProyectoFinal/src/PDF/FilePDF.java
+++ ProyectoFinal/src/PDF/FilePDF.java
@@ -1,5 +1,10 @@
 package PDF;
 
+import Domain.BST;
+import Domain.BTreeNode;
+import Domain.CircularLinkList;
+import Domain.ListException;
+import Objects.Product;
 import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
@@ -20,11 +25,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.scene.control.Cell;
 
 public class FilePDF {
-
     //Constructor
+
     public FilePDF() {
 
     }
@@ -45,14 +49,14 @@ public class FilePDF {
     }
 
     //Genera el pdf y escribe lo que queremos
-    public void productsAndFood(String fileName) throws FileNotFoundException, DocumentException, BadElementException, URISyntaxException, IOException {
+    public void productsAndFood(String fileName, BST treeProducts, BST treeFood) throws FileNotFoundException, DocumentException, BadElementException, URISyntaxException, IOException, ListException {
         FileOutputStream file = new FileOutputStream(fileName + ".pdf");
         Document document = new Document();
         PdfWriter.getInstance(document, file);
 
         //Instancia para poder hacer la img, importante poner la imagen en la carpeta img
-        Image header = Image.getInstance("src/img/products&food.png");
-        header.scaleToFit(150, 250);
+        Image header = Image.getInstance("src/img/products&food.jpg");
+        header.scaleToFit(250, 600);
         header.setAlignment(Chunk.ALIGN_CENTER);
         //Se abre el documento para poder escribir en el
         document.open();
@@ -71,17 +75,30 @@ public class FilePDF {
         table.addCell("Name");
         table.addCell("$Price");
         table.addCell("Supermarket");
-        
-//        try {
-//            for (int i = 1; i <= list.size(); i++) {
-//                Career career = (Career) list.getNode(i).data;
-//                table.addCell(String.valueOf(career.getId()));
-//                table.addCell(career.getDescription());
-//            }
-//            document.add(table);//Agrega la tabla al documento
-//
-//        } catch (ListException | DocumentException e) {
-//        }
+
+        CircularLinkList arrayListTemp = new CircularLinkList();
+        CircularLinkList arrayList = tourTree(treeProducts.getRoot(), arrayListTemp);
+
+        if (!arrayList.isEmpty()) {
+            try {
+                for (int i = 1; i <= arrayList.size(); i++) {
+                    Product product = (Product) arrayList.getNode(i).data;
+                    table.addCell(String.valueOf(product.getID()));
+                    table.addCell(product.getName());
+                    table.addCell(String.valueOf(product.getPrice()));
+                    table.addCell(String.valueOf(product.getSupermarketID()));//Cambiar para que aparezca el supermarcado
+                }
+                document.add(table);//Agrega la tabla al documento
+
+            } catch (ListException | DocumentException e) {
+            }
+        } else {
+            Paragraph parrafoTemp = new Paragraph();
+            parrafoTemp.setAlignment(Paragraph.ALIGN_CENTER);
+            parrafoTemp.add("\n\nThere's no products registered\n\n");
+            document.add(table);//Agrega la tabla al documento 
+            document.add(parrafoTemp);
+        }
 
         //FOOD
         Paragraph parrafo2 = new Paragraph();
@@ -97,51 +114,133 @@ public class FilePDF {
         table2.addCell("$Price");
         table2.addCell("Restaurant");
 
+        CircularLinkList arrayListTemp2 = new CircularLinkList();
+        CircularLinkList arrayList2 = tourTree(treeFood.getRoot(), arrayListTemp2);
+
+        if (!arrayList2.isEmpty()) {
+            try {
+                for (int i = 1; i <= arrayList2.size(); i++) {
+                    Product product = (Product) arrayList2.getNode(i).data;
+                    table2.addCell(String.valueOf(product.getID()));
+                    table2.addCell(product.getName());
+                    table2.addCell(String.valueOf(product.getPrice()));
+                    table2.addCell(String.valueOf(product.getSupermarketID()));//Cambiar para que aparezca el restaurante
+                }
+                document.add(table2);//Agrega la tabla al documento
+
+            } catch (ListException | DocumentException e) {
+            }
+        } else {
+            Paragraph parrafoTemp = new Paragraph();
+            parrafoTemp.setAlignment(Paragraph.ALIGN_CENTER);
+            parrafoTemp.add("\nThere's no food registered\n");
+            document.add(table2);//Agrega la tabla al documento
+            document.add(parrafoTemp);
+        }
+
         //Importante cerrar el pdf
         document.close();
     }
 
+    private CircularLinkList tourTree(BTreeNode node, CircularLinkList arrayList2) {
+        if (node != null) {
+            tourTree(node.left, arrayList2);
+            Product p = (Product) node.data;
+            arrayList2.add(p);
+            tourTree(node.right, arrayList2);
+        }
+        return arrayList2;
+    }
+
     //Genera el pdf y escribe lo que queremos
-    public void studentPDF(String fileName) throws FileNotFoundException, DocumentException, BadElementException, URISyntaxException, IOException {
+    public void restaurantsAndSupermarket(String fileName) throws FileNotFoundException, DocumentException, BadElementException, URISyntaxException, IOException, ListException {
         FileOutputStream file = new FileOutputStream(fileName + ".pdf");
         Document document = new Document();
         PdfWriter.getInstance(document, file);
 
         //Instancia para poder hacer la img, importante poner la imagen en la carpeta img
-        Image header = Image.getInstance("src/img/logo-ucr.png");
-        header.scaleToFit(150, 250);
+        Image header = Image.getInstance("src/img/restaurants&supermarkets.jpg");
+        header.scaleToFit(250, 600);
         header.setAlignment(Chunk.ALIGN_CENTER);
         //Se abre el documento para poder escribir en el
         document.open();
         document.add(header);//Se agrega la img
 
+        //SUPERMARKETS
         Paragraph parrafo = new Paragraph();
         parrafo.setAlignment(Paragraph.ALIGN_CENTER);
         parrafo.setFont(FontFactory.getFont("Tahoma", 18, Font.BOLD, BaseColor.BLACK));
-        parrafo.add("\n\nRegistered Students\n\n");
+        parrafo.add("\n\nRegistered Supermarkets\n\n");
         document.add(parrafo);
 
-//        try {
-//            for (int i = 1; i <= list.size(); i++) {
-//                Paragraph parrafo1 = new Paragraph();
-//                Student student = (Student) list.getNode(i).data;
-//                parrafo1.add("\nIdentification: " + String.valueOf(student.getId()));
-//                parrafo1.add("\nStudent ID: " + student.getStudentID());
-//                parrafo1.add("\nLast Name: " + student.getLastname());
-//                parrafo1.add("\nFirst Name: " + student.getFirstname());
-//                parrafo1.add("\nBirthday: " + Util.Utility.dateFormat(student.getBirthday2()));
-//                parrafo1.add("\nPhone Number: " + student.getPhoneNumber());
-//                parrafo1.add("\nEmail: " + student.getEmail());
-//                parrafo1.add("\nAddress: " + student.getAddress());
-//                parrafo1.add("\nCareer: " + Util.Utility.getCarrerByID(String.valueOf(student.getCareerID())).getDescription());
-//                parrafo1.add("\n----------------------------------------");
-//                document.add(parrafo1);
-//            }
+        //Tabla
+        PdfPTable table = new PdfPTable(3);//Columnas y nombres
+        table.addCell("Identificator");
+        table.addCell("Name");
+        table.addCell("Location");
+
+//        CircularLinkList arrayListTemp = new CircularLinkList();
+//        CircularLinkList arrayList = tourTree(treeProducts.getRoot(), arrayListTemp);
+//        if (!arrayList.isEmpty()) {
+//            try {
+//                for (int i = 1; i <= arrayList.size(); i++) {
+//                    Product product = (Product) arrayList.getNode(i).data;
+//                    table.addCell(String.valueOf(product.getID()));
+//                    table.addCell(product.getName());
+//                    table.addCell(String.valueOf(product.getPrice()));
+//                    table.addCell(String.valueOf(product.getSupermarketID()));//Cambiar para que aparezca el supermarcado
+//                }
+//                document.add(table);//Agrega la tabla al documento
 //
-//        } catch (ListException | DocumentException e) {
+//            } catch (ListException | DocumentException e) {
+//            }
+//        } else {
+//            Paragraph parrafoTemp = new Paragraph();
+//            parrafoTemp.setAlignment(Paragraph.ALIGN_CENTER);
+//            parrafoTemp.add("\n\nThere's no products registered\n\n");
+        document.add(table);//Agrega la tabla al documento 
+//            document.add(parrafoTemp);
 //        }
+//
+        //RESTAURANTS
+        Paragraph parrafo2 = new Paragraph();
+        parrafo2.setAlignment(Paragraph.ALIGN_CENTER);
+        parrafo2.setFont(FontFactory.getFont("Tahoma", 18, Font.BOLD, BaseColor.BLACK));
+        parrafo2.add("\n\nRegistered Restaurants\n\n");
+        document.add(parrafo2);
+
+        //Tabla
+        PdfPTable table2 = new PdfPTable(3);//Columnas y nombres
+        table2.addCell("Identificator");
+        table2.addCell("Name");
+        table2.addCell("Location");
+//
+//        CircularLinkList arrayListTemp2 = new CircularLinkList();
+//        CircularLinkList arrayList2 = tourTree(treeFood.getRoot(), arrayListTemp2);
+//
+//        if (!arrayList2.isEmpty()) {
+//            try {
+//                for (int i = 1; i <= arrayList2.size(); i++) {
+//                    Product product = (Product) arrayList2.getNode(i).data;
+//                    table2.addCell(String.valueOf(product.getID()));
+//                    table2.addCell(product.getName());
+//                    table2.addCell(String.valueOf(product.getPrice()));
+//                    table2.addCell(String.valueOf(product.getSupermarketID()));//Cambiar para que aparezca el restaurante
+//                }
+//                document.add(table2);//Agrega la tabla al documento
+//
+//            } catch (ListException | DocumentException e) {
+//            }
+//        } else {
+//            Paragraph parrafoTemp = new Paragraph();
+//            parrafoTemp.setAlignment(Paragraph.ALIGN_CENTER);
+//            parrafoTemp.add("\nThere's no food registered\n");
+        document.add(table2);//Agrega la tabla al documento
+//            document.add(parrafoTemp);
+//        }
+
         //Importante cerrar el pdf
         document.close();
     }
-
+    
 }//end class
